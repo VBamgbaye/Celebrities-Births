e8fb330b0623dad87ad779e82651354d5236e8d4
VBamgbaye <bamgbayev@gmail.com>
2021-10-06 05:58:50 +0100

    New version build

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='Youtube_scrapper',
-    version='0.0.3',
+    version='0.0.4',
     description='A tool for extracting youtube video details',
     long_description=long_description,
     long_description_content_type='text/markdown',
